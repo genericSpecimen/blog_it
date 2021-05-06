cb1f4c6c799f0e4225e0fadcdaacff884c41d424
Irfan Sheikh <irfan.sh3112@gmail.com>
2021-05-06 12:44:38 +0530

    created a basic homepage which display site statistics

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ A blogging platform made with Django.
 - [x] Create models for BlogAuthor, BlogPost, BlogComment.
 - [x] Register the models and add test objects using the admin site.
 - [x] Customize the admin site.
-- [ ] Create the home page.
+- [x] Create the home page.
 - [ ] Add the concept of a user and a blog post.
 - [ ] Add the functionality of commenting on blog posts.
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import BlogAuthor, BlogPost, BlogComment
+
+def index(request):
+    """
+    View function for the home page of the website.
+    """
+    
+    num_blogauthors = BlogAuthor.objects.all().count()
+    num_blogposts = BlogPost.objects.all().count()
+    num_blogcomments = BlogComment.objects.all().count()
+    
+    context = {
+        'num_blogauthors' : num_blogauthors,
+        'num_blogposts' : num_blogposts,
+        'num_blogcomments': num_blogcomments,
+    }
+    
+    return render(request, 'index.html', context=context)
