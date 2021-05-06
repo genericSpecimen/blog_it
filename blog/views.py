7846337401ee4ab15bcccd9d975ee84588633bdd
Irfan Sheikh <irfan.sh3112@gmail.com>
2021-05-06 12:54:53 +0530

    added number of visits statistic in homepage

--- blog/views.py
+++ blog/views.py
@@ -12,11 +12,15 @@ def index(request):
     num_blogauthors = BlogAuthor.objects.all().count()
     num_blogposts = BlogPost.objects.all().count()
     num_blogcomments = BlogComment.objects.all().count()
+
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
     
     context = {
-        'num_blogauthors' : num_blogauthors,
-        'num_blogposts' : num_blogposts,
-        'num_blogcomments': num_blogcomments,
+        'num_blogauthors'   : num_blogauthors,
+        'num_blogposts'     : num_blogposts,
+        'num_blogcomments'  : num_blogcomments,
+        'num_visits'        : num_visits
     }
     
     return render(request, 'index.html', context=context)
